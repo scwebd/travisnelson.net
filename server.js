b5a6fadd8d93103abe519e7b021902049c8eef58
Travis Nelson <tnelson@placeable.com>
2015-08-04 09:05:21 -0600

    Initial theme commit using Kedavra
    https://wrapbootstrap.com/theme/kedavra-multipurpose-2-themes-in-1-WB024
    95S4

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ var _ = require('underscore');
 var port = process.env.PORT || 1337;
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var hbs = exphbs.create({
+	defaultLayout: 'main',
+    helpers: {
+        copyright: function () { return new Date().getFullYear(); }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) { res.render('home', {test:"test1"}) });
